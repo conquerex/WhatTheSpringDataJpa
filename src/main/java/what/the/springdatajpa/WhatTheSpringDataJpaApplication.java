5bd74f71b98d511983eff20530fbff3ee8bfe070
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2021-06-10 14:54:44 +0900

    init

---
+++ src/main/java/what/the/springdatajpa/WhatTheSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package what.the.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatTheSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatTheSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/what/the/springdatajpa/WhatTheSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package what.the.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatTheSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
